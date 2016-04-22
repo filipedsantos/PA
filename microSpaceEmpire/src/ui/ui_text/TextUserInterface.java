46766cf8f9de484bb3fa9eeefd26163845190cd1
migas06 <miguelmartins018@gmail.com>
2016-04-22 21:08:36 +0100

    22-04 21h

--- microSpaceEmpire/src/ui/ui_text/TextUserInterface.java
+++ microSpaceEmpire/src/ui/ui_text/TextUserInterface.java
@@ -3,6 +3,9 @@ package ui.ui_text;
 
 import java.util.Scanner;
 import model.Game;
+import model.states.AwaitBeginning;
+import model.states.AwaitOption;
+import model.states.Ending;
 
 public class TextUserInterface {
     
@@ -15,7 +18,41 @@ public class TextUserInterface {
     }
 
     public void run() {
-        System.out.println("Running..");
+        
+        while(!(game.getState() instanceof Ending)){
+            if(game.getState() instanceof AwaitBeginning)
+                getUserInputWhileAwaitingBeginning();
+            if(game.getState() instanceof AwaitOption)
+                getUserInputWhileAwaitingOption();
+        }
+        
+        System.out.println("");
+        System.out.println("Game Over");
     }
     
+    //incompleto
+    public void getUserInputWhileAwaitingBeginning(){
+        System.out.println("estive no primeiro estado vou sair para o proximo");
+        game.start();
+    }
+    
+    //incompleto
+    public void getUserInputWhileAwaitingOption(){
+        int opt;
+        
+        System.out.println("1 - Explore-Attack");
+        System.out.println("2 - Conquer");
+        System.out.println("3 - Pass");
+        
+        while(this.s.hasNextInt()) 
+            s.next();
+            
+        opt = s.nextInt();
+        
+        if(opt == 3)
+            game.pass();
+        /*****
+          restantes submenus aqui
+        *****/
+    }
 }
